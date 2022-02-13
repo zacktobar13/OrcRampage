509f864ddbac6124114657951456c06f7d91c863
Taylor Moore <tjosephmoore@gmail.com>
2022-02-12 21:12:32 -0800

    New art for archers. Fixed a lot of sprite ordering issues. Changed how chests work to no longer need colliders. Deleted some old assets.

--- Assets/Resources/Scripts/Interactibles/Chest.cs
+++ Assets/Resources/Scripts/Interactibles/Chest.cs
@@ -1,68 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Chest : MonoBehaviour
 {
-    public int openCost;
     public GameObject copperCoin;
     public GameObject silverCoin;
     public GameObject goldCoin;
+    public float openDistance;
+    public int keysRequired = 0;
+    public bool debug;
 
+    Transform playerTransform;
     Animator animator;
-    bool isOpen;
 
-    public TextMeshProUGUI costText;
-
-    void Start()
-    {
+    void Start() {
         animator = GetComponent<Animator>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
     }
 
-	private void Update()
-	{
-        if (!isOpen && costText.isActiveAndEnabled && PlayerInput.interact)
-        {
-            AttemptOpen(0);
-        }
-	}
-
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-        if (!isOpen)
-        {
-            costText.enabled = collision.tag == "Player"; 
+    private void Update() {
+        if (PlayerInput.interact && Vector2.Distance(transform.position, playerTransform.position) <= openDistance) {
+            AttemptOpen();
         }
     }
 
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-        if (!isOpen)
-        {
-            costText.enabled = false;
-        }
-    }
+    public void AttemptOpen() {
+        animator.SetBool("isOpen", true);
 
-	public void AttemptOpen(int playerCurrency)
-    {
-        if (playerCurrency >= openCost)
-        {
-            animator.SetBool("isOpen", true);
-            isOpen = true;
-            for (int i = 0; i < 10; i++)
-            {
-                Instantiate(copperCoin, transform.position, Quaternion.identity);
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                Instantiate(silverCoin, transform.position, Quaternion.identity);
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                Instantiate(goldCoin, transform.position, Quaternion.identity);
-            }
-            costText.enabled = false;
+        for (int i = 0; i < 10; i++) {
+            Instantiate(copperCoin, transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            Instantiate(silverCoin, transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            Instantiate(goldCoin, transform.position, Quaternion.identity);
         }
     }
 }
+
